898c46536870926f7d62ed5c51f9404b6aaf308a
Zachary Collier <zacharycollier36@protonmail.com>
2025-08-12 21:44:15 -0230

    finalized frontend - it's simple, but it should work fine

--- src/components/EnterNumbers.jsx
+++ src/components/EnterNumbers.jsx
@@ -1,5 +1,143 @@
+import { useState, useRef, useEffect } from "react";
+
 // Enter numbers into a box, then send that list of numbers to the API
 
-function EnterNumbers() {}
+function EnterNumbers() {
+	const [numberArray, setNumberArray] = useState([]);
+
+	const [currentNumber, setCurrentNumber] = useState();
+
+	const [errorMessage, setErrorMessage] = useState({
+		type: "empty",
+		message: "",
+	});
+
+	const [root, setRoot] = useState(null);
+
+	const alert = useRef(null);
+
+	useEffect(() => {
+		alert.current.innerHTML = `<p>${errorMessage.message}</p>`;
+		if (errorMessage.type === "empty") {
+			alert.current.style.background = "rgba(0,0,0,0)";
+		} else if (errorMessage.type === "error") {
+			alert.current.style.background = "red";
+		} else {
+			alert.current.style.background = "green";
+		}
+
+		if (errorMessage.type != "empty") {
+			setTimeout(() => {
+				setErrorMessage({ type: "empty", message: "" });
+			}, 5000);
+		}
+	}, [errorMessage]);
+
+	const tree = useRef(null);
+
+	useEffect(() => {
+		tree.current.innerHTML = `<p>${root}</p>`;
+	}, [root]);
+
+	const handleNumberChange = (event) => {
+		event.preventDefault();
+		setCurrentNumber(event.target.value);
+	};
+
+	const handleNumberSubmit = (event) => {
+		event.preventDefault();
+		if (isNaN(currentNumber)) {
+			setErrorMessage({
+				type: "error",
+				message: "ERR: Not A Number.",
+			});
+		} else {
+			setErrorMessage({
+				type: "empty",
+				message: "",
+			});
+			setNumberArray(numberArray.push(currentNumber));
+		}
+	};
+
+	const handleArraySubmit = (event) => {
+		event.preventDefault();
+		//Send the list to the API
+		fetch("http://localhost:8080/binary-search-tree/process-numbers", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(numberArray),
+		})
+			.then((response) => response.json())
+			.then((data) => setRoot(data))
+			.catch((error) =>
+				setErrorMessage({
+					type: "error",
+					message: "ERR: " + error,
+				})
+			);
+	};
+
+	const handleArraySubmitBalance = (event) => {
+		event.preventDefault();
+		//Send the list to the API
+		fetch("http://localhost:8080/binary-search-tree/process-numbers/balance", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(numberArray),
+		})
+			.then((response) => response.json())
+			.then((data) => setRoot(data))
+			.catch((error) =>
+				setErrorMessage({
+					type: "error",
+					message: "ERR: " + error,
+				})
+			);
+	};
+
+	const handleArrayReset = (event) => {
+		event.preventDefault();
+		setNumberArray([]);
+	};
+
+	return (
+		<main>
+			<h2>Enter Numbers</h2>
+			<p>
+				Enter numbers into a box and press enter. They will show up on the list.
+				Once you're happy with all the numbers, press submit.
+			</p>
+			<div className="alertbox" ref={alert}></div>
+			<form onSubmit={handleNumberSubmit}>
+				<label htmlFor="numberinput">Number Input: </label>
+				<input
+					type="number"
+					id="numberinput"
+					name="numberinput"
+					value={currentNumber}
+					onChange={handleNumberChange}
+				/>
+				<input type="submit" />
+			</form>
+			<p>
+				Current numbers:
+				{numberArray.map((number, index) => {
+					return number + ", ";
+				})}
+			</p>
+			<form onSubmit={handleArraySubmit}>
+				<input type="submit" />
+			</form>
+			<form onSubmit={handleArraySubmitBalance}>
+				<input type="submit" />
+			</form>
+			<form onReset={handleArrayReset}>
+				<input type="reset" />
+			</form>
+			<div className="treebox" ref={tree}></div>
+		</main>
+	);
+}
 
 export default EnterNumbers;
--- src/components/PreviousTrees.jsx
+++ src/components/PreviousTrees.jsx
@@ -1,5 +1,36 @@
+import { useState, useRef, useEffect } from "react";
+
 // Get trees from API
 
-function PreviousTrees() {}
+function PreviousTrees() {
+	const [trees, setTrees] = useState([]);
+
+	const tree = useRef(null);
+
+	useEffect(() => {
+		fetch("http://localhost:8080/binary-search-tree", {
+			method: "GET",
+			headers: { "Content-Type": "application/json" },
+		})
+			.then((response) => response.json())
+			.then((data) => setTrees(data))
+			.catch((error) =>
+				setErrorMessage({
+					type: "error",
+					message: "ERR: " + error,
+				})
+			);
+
+		tree.current.innerHTML = `<p>${trees}</p>`;
+	}, []);
+
+	return (
+		<main>
+			<h2>Previous Trees</h2>
+			<p>These trees are from all prior uses of this app.</p>
+			<div className="treebox" ref={tree}></div>
+		</main>
+	);
+}
 
 export default PreviousTrees;
